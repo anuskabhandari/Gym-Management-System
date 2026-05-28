0ac2b21f8294957810dff5543d9ef6bd2db51f2d
anuskabhandari <bhandarirekha652@gmail.com>
2026-05-28 09:45:55 +0545

    first commit

---
+++ member/api/views.py
@@ -0,0 +1,44 @@
+from member.models import Member
+from member.api.serializer import MemberSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+@api_view(['GET'])
+def memberlist(request):
+    data= Member.objects.all()
+    serializer = MemberSerializer(data , many = True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def membercreate(request):
+    post_data = request.data
+    serializer = MemberSerializer(data=post_data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message":"Member Successfully created"
+        },201)
+    else:
+        return Response(serializer.errors,422)
+    
+
+@api_view(['PUT'])
+def memberupdate(request,id):
+    member = Member.objects.get(id=id)
+    serializer = MemberSerializer(member, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({
+            "message":"Member Successfully updated"
+        }, status.HTTP_200_OK)
+    else:
+        return Response(serializer.errors,status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+@api_view(['DELETE'])
+def memberdelete(request,id):
+    member = Member.objects.filter(id=id)
+    member.delete()
+    return Response({
+        "message":"Member successfully deleted"
+    }, 204)    
